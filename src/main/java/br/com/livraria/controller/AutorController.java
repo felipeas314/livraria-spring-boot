b172719c2d32b7bd231bf60dfed000fd9de4a9a8
Felipe Alexandre <felipeas314@gmail.com>
2019-09-14 22:43:35 -0300

    crud de autor

--- src/main/java/br/com/livraria/controller/AutorController.java
+++ src/main/java/br/com/livraria/controller/AutorController.java
@@ -1,5 +1,8 @@
 package br.com.livraria.controller;
 
+import java.net.URI;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
@@ -7,11 +10,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.livraria.domain.Autor;
 import br.com.livraria.repository.AutorRepository;
@@ -26,34 +33,50 @@ public class AutorController {
 		this.autorRepository = autorRepository;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "")
+	@PostMapping
 	public ResponseEntity<Autor> adiciona(@Valid @RequestBody Autor autor, BindingResult bindingResult) {
-		return null;
+
+		URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(autor.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).body(autor);
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/todos")
-	public ResponseEntity<Page<Autor>> listaTodos(@PageableDefault Pageable pageable) {
+	@GetMapping("/lista/paginacao")
+	public ResponseEntity<Page<Autor>> listaComPaginacao(@PageableDefault Pageable pageable) {
 		return ResponseEntity.ok(autorRepository.findAll(pageable));
 	}
 
-	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+	@GetMapping("/lista/todos")
+	public ResponseEntity<List<Autor>> listaTodos() {
+		return ResponseEntity.ok(autorRepository.findAll());
+	}
+
+	@GetMapping("/buscaporid/{id}")
+	public ResponseEntity<Autor> buscaPorId(@PathVariable("id") Integer id) {
+		return autorRepository.findById(id).map(categoria -> {
+			return ResponseEntity.ok(categoria);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/atualiza/{id}")
+	public ResponseEntity<Autor> atualiza(@RequestBody Autor autor, @PathVariable("id") Integer id) {
+		return autorRepository.findById(id).map(a -> {
+
+			a.setNome(autor.getNome());
+			a.setSobre(autor.getSobre());
+			a.setDataDeNascimento(autor.getDataDeNascimento());
+
+			return ResponseEntity.ok(a);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/deleta/{id}")
 	public ResponseEntity<?> remove(@PathVariable("id") Integer id) {
-		return null;
-	}
-
-	@RequestMapping(method = RequestMethod.PUT, value = "")
-	public ResponseEntity<?> atualiza(@RequestBody Autor autor) {
-		return null;
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
-	public ResponseEntity<?> buscaPorId(@PathVariable("id") Integer id) {
-		return null;
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "")
-	public ResponseEntity<?> listaComPaginacao() {
-		return null;
+		return autorRepository.findById(id).map(autor -> {
+			autorRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}).orElse(ResponseEntity.notFound().build());
 	}
 
 }
--- src/main/java/br/com/livraria/domain/Autor.java
+++ src/main/java/br/com/livraria/domain/Autor.java
@@ -30,6 +30,37 @@ public class Autor {
 
 	private String sobre;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -60,6 +91,12 @@ public class Autor {
 
 	public void setSobre(String sobre) {
 		this.sobre = sobre;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [id=" + id + ", nome=" + nome + ", email=" + email + ", dataDeNascimento=" + dataDeNascimento
+				+ ", sobre=" + sobre + "]";
 	}
 
 }
